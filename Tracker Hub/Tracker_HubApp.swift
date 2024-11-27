3935fe11027228334e56048b81def38fc736b6e3
nikita <nikita.d.smirnov@gmail.com>
2024-11-28 01:50:16 +0300

    Initial Commit

---
+++ Tracker Hub/Tracker_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_HubApp.swift
+//  Tracker Hub
+//
+//  Created by Никита Смирнов on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tracker_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tracker HubTests/Tracker_HubTests.swift
@@ -0,0 +1,16 @@
+//
+//  Tracker_HubTests.swift
+//  Tracker HubTests
+//
+//  Created by Никита Смирнов on 28.11.2024.
+//
+
+import Testing
+
+struct Tracker_HubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
